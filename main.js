c250aa62172d6e07c14c846a648e0147963195bd
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-09-14 16:15:53 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
 function setup() {
     canvas = createCanvas(250, 250);
     canvas.center();
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speachSynthesis;
+}
+
+function preload() {
+    classifier = ml5.imageClassifier("DoodleNet");
 }
 
 function clearCanvas() {
     background("white");
+}
+
+function draw() {
+    strokeWeight(11);
+    stroke(0);
+
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    {
+        console.log(results);
+        document.getElementById("lable").innerHTML = "I guess " + results[0].label;
+        document.getElementById("confidence").innerHTML = "I am " + results[0].confidence + Math.round(results[0].confidence * 100) + "% confident";
+
+        utterThis = new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utterThis);
+    }
 }
